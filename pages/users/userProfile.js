1631d6d356b1227c8240fd147529fa2f899c1c49
Chowdhury Md. Rabby Mahmud <md.rabby.mahmud@gmail.com>
2023-11-13 13:10:18 +0600

    profile picture uploaded feature

--- pages/users/userProfile.js
+++ pages/users/userProfile.js
@@ -1,17 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 //redux imports
 import { connect } from "react-redux";
 
 // Theme imports
 import { tokens } from "../theme";
-import { Typography, useTheme } from "@mui/material";
+
+// bootstarp
+import { 
+  Typography, 
+  useTheme, 
+  Button,
+  Checkbox,
+  FormControlLabel,
+  TextField,
+  MenuItem, } from "@mui/material";
+
+// axios
+import axios from "axios";
+
+//Base url
+import { BASE_URL } from "../../base";
 
 const userProfile = ({ user }) => {
   // console.log("user");
   // console.log(user);
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const [photo, setPhoto] = useState("");
+  const [check, setCheck] = useState(0);
   // useEffect(() => {
   //   console.log("jwtDecode(user.token)");
   //   console.log(jwtDecode(user.token));
@@ -22,6 +39,72 @@ const userProfile = ({ user }) => {
   //   console.log(current_user);
   // }, []);
 
+  const submit = () => {
+    const profileData = {
+      photo
+    };
+    console.log("profileData");
+    console.log(profileData);
+    const apiProfile = BASE_URL + "upload/image";
+    const config = {
+      headers: { Authorization: `Bearer ${user?.token}` },
+    };
+    if (photo == "") {
+      alert("Please select an Image!");
+    } else {
+      axios
+        .post(apiProfile, profileData, config)
+        .then((response) => {
+          if (response.data.status == 200) {
+            alert("Profile Photo Uploaded!");
+            setCheck(3)
+          } else {
+            setFormErrors(Object.values(response.data.errors));
+            console.log(response.data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          setCheck(3);
+        });
+    }
+  };
+
+  // PHOTO
+  const onChange = async (e) => {
+    let files = e.target.files || e.dataTransfer.files;
+    // console.log(files);
+    if (files.length > 0) {
+      uploadDocuments(e, files[0]);
+    }
+  };
+
+  const uploadDocuments = async (event, file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = async () => {
+        try {
+          const response = submitFile(reader.result, file.name);
+          resolve(response);
+        } catch (err) {
+          reject(err);
+        }
+      };
+      reader.onerror = (error) => {
+        reject(error);
+      };
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const submitFile = (result, name) => {
+    // console.log(result, name);
+    console.log("result");
+    console.log(result);
+    setPhoto(result);
+    setCheck(1)
+  };
+
   return (
     <div className="row">
       <div className="col-md-4 text-center">
@@ -29,7 +112,8 @@ const userProfile = ({ user }) => {
           alt="profile-user"
           width={200}
           height={200}
-          src={`../../assets/images/user.png`}
+          src={photo || `../../assets/images/user.png`}
+          // src={`../../assets/images/user.png`}
           style={{ cursor: "pointer", borderRadius: "50%" }}
         />
         <Typography
@@ -44,6 +128,33 @@ const userProfile = ({ user }) => {
           className="mt-3"
           color={colors.grey[300]}
         >
+        {!(check == 1) && <Button
+          variant="contained"
+          component="label"
+          color="secondary"
+          fullWidth
+        >
+          <input
+          hidden
+          accept="image/*"
+          multiple
+          type="file"
+          onChange={onChange}
+          />
+          Change Profile Picture
+        </Button>
+        }
+        {!(check == 0 || check == 3) && <Button
+          variant="contained"
+          component="label"
+          color="secondary"
+          fullWidth
+          onClick={submit}
+        >
+          Confirm Change Profile Picture
+        </Button>
+        }
+          
           {/* {user?.roles[0]?.name} */}
         </Typography>
       </div>
