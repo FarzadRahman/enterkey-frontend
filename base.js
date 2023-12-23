3ed974d9ec7f69293d86845367f7696dbd9ca30f
Farzad Rahman <farzadrahman59@gmail.com>
2023-12-23 13:04:16 +0600

    Farzad

--- base.js
+++ base.js
@@ -1,7 +1,11 @@
 // export const BASE_URL = "http://13.213.35.25/";
 // export const BASE_URL = "http://18.139.226.72/";
 
-export const BASE_URL = "https://dpcsict.xyz/backend/api/";
-export const IMAGE_URL = "https://dpcsict.xyz/backend/public/profile-picture/";
-export const SIGN_URL = "https://dpcsict.xyz/backend/public/signature/";
-// export const BASE_URL="http://localhost:8000/api/";
+// export const BASE_URL = "http://dpcsict.xyz/backend/api/";
+// export const IMAGE_URL = "https://dpcsict.xyz/backend/public/profile-picture/";
+// export const SIGN_URL = "https://dpcsict.xyz/backend/public/signature/";
+
+export const BASE_URL = "https://saikat.mndmbera.edu.bd/api/";
+export const IMAGE_URL = "https://saikat.mndmbera.edu.bd/public/profile-picture/";
+export const SIGN_URL = "https://saikat.mndmbera.edu.bd/public/signature/";
+
