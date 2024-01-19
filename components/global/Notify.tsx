d7afbd1532b3cff3f8a59769745781a629278526
Sofen <md.rabby.mahmud@gmail.com>
2024-01-19 13:11:20 +0600

    deploy config

--- components/global/Notify.tsx
+++ components/global/Notify.tsx
@@ -131,7 +131,7 @@ export default function DescriptionAlerts() {
                         )
                       }
                     >
-                      {notification.content}
+                      {/* {notification.content} */}
                     </Alert>
                   );
                 })}
